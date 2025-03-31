c7d03ba11a1b4baaf307e9250b94e05a9f09abf1
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-03-31 08:03:01 +0700

    latest

---
+++ src/MainApplication.java
@@ -0,0 +1,8 @@
+
+import view.LoginForm;
+
+public class MainApplication {
+	public static void main(String[] args) {
+		LoginForm.main(null);
+	}
+}
--- src/test/maintest.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import view.LoginForm;
-
-public class maintest {
-	public static void main(String[] args) {
-		LoginForm.main(null);
-	}
-}
